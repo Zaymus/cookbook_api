b088dbc17c90d8b0a64962f2987153477b2c6935
Carson Brown <carsonbrown47@gmail.com>
2022-08-29 14:51:06 -0400

    completed recipe API CRUD structure

--- routes/recipes.js
+++ routes/recipes.js
@@ -4,7 +4,6 @@ const router = express.Router();
 const Recipe = require("../models/recipe");
 
 router.post("/recipe", (req, res, next) => {
-	console.log(req.body);
 	const name = req.body.name;
 	const numServings = req.body.numServings;
 	const steps = req.body.steps;
@@ -20,23 +19,24 @@ router.post("/recipe", (req, res, next) => {
 		.save()
 		.then((result) => {
 			console.log("Saved Recipe!");
-			res.json({ status: "Saved" });
+			res.json({ ...req.json, status: "Saved" });
 		})
 		.catch((err) => {
 			console.log(err);
-			res.json({ status: "error" });
+			res.json({ ...req.json, isSuccessful: false, err });
 		});
 });
 
 router.get("/recipe/:recipeId", (req, res, next) => {
 	const id = req.params.recipeId;
-	console.log("id");
 	Recipe.findById(id)
 		.then((recipe) => {
-			res.json(200, recipe);
+			if (recipe == null) res.json({ ...req.json, isSuccessful: false });
+			res.json({ ...req.json, wasFound: true, recipe });
 		})
 		.catch((err) => {
 			console.log(err);
+			res.json({ ...req.json, isSuccessful: false, err });
 		});
 });
 
@@ -44,10 +44,53 @@ router.get("/recipe", (req, res, next) => {
 	const name = req.body.name;
 	Recipe.find({ name: name })
 		.then((recipe) => {
-			res.json(recipe);
+			if (!recipe.length) res.json({ ...req.json, isSuccessful: false });
+			res.json({ ...req.json, wasFound: true, recipe });
 		})
 		.catch((err) => {
 			console.log(err);
+			res.json({ ...req.json, isSuccessful: false, err });
+		});
+});
+
+router.patch("/recipe", (req, res, next) => {
+	const id = req.body.id;
+	const name = req.body.name;
+	const numServings = req.body.numServings;
+	const steps = req.body.steps;
+	const imageUrl = req.body.imageUrl;
+	Recipe.findById(id)
+		.then((recipe) => {
+			recipe.name = name;
+			recipe.numServings = numServings;
+			recipe.steps = steps;
+			recipe.imageUrl = imageUrl;
+			recipe
+				.save()
+				.then((result) => {
+					console.log("Updated Recipe!");
+					res.json({ ...req.json, wasFound: true, wasUpdated: true });
+				})
+				.catch((err) => {
+					console.log(err);
+					res.json({ ...req.json, isSuccessful: false, err });
+				});
+		})
+		.catch((err) => {
+			console.log(err);
+			res.json({ ...req.json, isSuccessful: false, err });
+		});
+});
+
+router.put("/recipe", (req, res, next) => {
+	const id = req.body.id;
+	Recipe.deleteOne({ _id: id })
+		.then((result) => {
+			res.json({ ...req.json, wasFound: true, wasDeleted: true });
+		})
+		.catch((err) => {
+			console.log(err);
+			res.json({ ...req.json, isSuccessful: false, err });
 		});
 });
 
