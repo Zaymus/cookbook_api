d687a546f05f3f358995444362913a8de578eadb
Carson Brown <carsonbrown47@gmail.com>
2022-08-30 02:34:45 -0400

    refined recipe model to support extra details and added user based attributes

--- models/recipe.js
+++ models/recipe.js
@@ -10,11 +10,68 @@ const recipeSchema = new Schema({
 		type: Number,
 		required: true,
 	},
-	steps: {
+	ingredients: [
+		{
+			ingredient: {
+				type: String,
+				required: true,
+			},
+			amount: {
+				type: Number,
+				required: true,
+			},
+			unit: {
+				type: String,
+				required: true,
+			},
+		},
+	],
+	steps: [
+		{
+			type: String,
+			required: true,
+		},
+	],
+	imageUrl: {
 		type: String,
 		required: true,
 	},
-	imageUrl: {
+	tags: [
+		{
+			type: String,
+		},
+	],
+	healthInfo: {
+		calories: {
+			type: Number,
+		},
+		macros: {
+			protien: {
+				type: Number,
+			},
+			carbs: {
+				type: Number,
+			},
+			fat: {
+				type: Number,
+			},
+		},
+		calorieType: {
+			type: String,
+		},
+	},
+	owner: {
+		type: Schema.Types.ObjectId,
+		ref: "User",
+		required: true,
+	},
+	users: [
+		{
+			type: Schema.Types.ObjectId,
+			ref: "User",
+		},
+	],
+	visibility: {
 		type: String,
 		required: true,
 	},
