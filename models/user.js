d2665e6b7a902e9a058efb7467d0548202db471c
Carson Brown <carsonbrown47@gmail.com>
2022-08-30 20:55:23 -0400

    added user data validation static methods ro user model

--- models/user.js
+++ models/user.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
-
+const { USER_PREFIX_LIST } = require("../util/constants");
 const Schema = mongoose.Schema;
+
+const userValidation = require("../util/constants").userValidation;
 
 const userSchema = new Schema({
 	email: {
@@ -37,5 +39,49 @@ const userSchema = new Schema({
 		},
 	],
 });
+
+userSchema.methods.verifyData = (userData) => {
+	verifyEmail();
+	verifyName();
+	verifyPassword();
+};
+
+userSchema.statics.verifyEmailFormat = (email) => {
+	return userValidation.EMAIL_FORMAT.test(email);
+};
+
+userSchema.statics.verifyName = (name) => {
+	const { prefix, firstName, middleName, lastName, suffix } = name;
+	isValidName =
+		userValidation.NAME_FORMAT.test(firstName) &&
+		userValidation.NAME_FORMAT.test(middleName) &&
+		userValidation.NAME_FORMAT.test(userValidation.lastName);
+
+	if (prefix?.length > 0) {
+		var prefixList = userValidation.USER_PREFIX_LIST.filter((p) => {
+			if (p === prefix) return p;
+		});
+		if (prefixList.length == 0) {
+			// invalid prefix
+			isValidName = false;
+		}
+	}
+
+	if (suffix?.length > 0) {
+		var suffixList = userValidation.USER_SUFFIX_LIST.filter((s) => {
+			if (s === suffix) return s;
+		});
+		if (suffixList.length == 0) {
+			// invalid suffix
+			isValidName = false;
+		}
+	}
+
+	return isValidName;
+};
+
+userSchema.statics.verifyPassword = (password) => {
+	return userValidation.PASSWORD_FORMAT.test(password);
+};
 
 module.exports = mongoose.model("User", userSchema);
