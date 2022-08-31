a159a9af3670c76782b6c5967e65487937e0c63a
Carson Brown <carsonbrown47@gmail.com>
2022-08-30 20:56:06 -0400

    added user validation rules

--- util/constants.js
+++ util/constants.js
@@ -10,6 +10,15 @@ SERVER_STATUS = {
 	UP: "up",
 };
 
-exports.SERVER_STATUS = SERVER_STATUS;
+const userValidation = {
+	EMAIL_FORMAT: new RegExp(/^([A-Za-z0-9])+@([A-Za-z0-9])+.([A-Za-z]{2,4})$/),
+	USER_PREFIX_LIST: ["Mr.", "Ms.", "Mrs.", "Miss.", "Dr."],
+	USER_SUFFIX_LIST: ["II", "III", "IV", "V", "Esq.", "Sr.", "Jr."],
+	NAME_FORMAT: /^[a-zA-Z]{2,40}$/,
+	PASSWORD_FORMAT:
+		/^(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/,
+};
 
 exports.CRUD_STATUS = CRUD_STATUS;
+exports.SERVER_STATUS = SERVER_STATUS;
+exports.userValidation = userValidation;
