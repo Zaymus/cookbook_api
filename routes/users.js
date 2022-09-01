9aec48c31fe4b46a16aac5f5324ec1e60b99d3af
Carson Brown <carsonbrown47@gmail.com>
2022-08-31 22:53:42 -0400

    Updated validate route to give deails on fail, completed CRUD routes for user, working on user recipe retrieval route

--- routes/users.js
+++ routes/users.js
@@ -1,20 +1,30 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
+const Recipe = require("../models/recipe");
 const CRUD_STATUS = require("../util/constants").CRUD_STATUS;
 const dotenv = require("dotenv");
 const axios = require("axios");
 const bcrypt = require("bcryptjs");
+const { userValidation } = require("../util/constants");
+const { default: mongoose } = require("mongoose");
 
 router.post("/user/validate", (req, res, next) => {
 	const { email, name, password } = req.body;
+	let isEmailValid = User.verifyEmailFormat(email);
+	let isNameValid = User.verifyName(name);
+	let isPasswordValid = User.verifyPassword(password);
 
-	let isValid =
-		User.verifyEmailFormat(email) &&
-		User.verifyName(name) &&
-		User.verifyPassword(password);
-
-	res.json({ dataVerified: isValid });
+	let isValid = isEmailValid && isNameValid && isPasswordValid;
+	if (isValid) {
+		res.json({ isDataVerified: isValid });
+	} else {
+		let errors = [];
+		if (!isEmailValid) errors.push("Email is invalid");
+		if (!isNameValid) errors.push("Name is invalid");
+		if (!isPasswordValid) errors.push("Password is invalid");
+		res.json({ isDataVerified: isValid, err: errors });
+	}
 });
 
 router.post("/user", (req, res, next) => {
@@ -28,7 +38,7 @@ router.post("/user", (req, res, next) => {
 			axios
 				.post(`${process.env.BASE_URL}/user/validate`, req.body)
 				.then((result) => {
-					if (result.data.dataVerified) {
+					if (result.data.isDataVerified) {
 						User.findOne({ email: email })
 							.then((user) => {
 								if (user == null) {
@@ -50,9 +60,9 @@ router.post("/user", (req, res, next) => {
 										.catch((err) => {
 											res.json({ ...req.json, err });
 										});
+								} else {
+									res.json({ ...req.json, err: "Email already in use" });
 								}
-								console.log(user);
-								res.json({ ...req.json, err: "Email already in use" });
 							})
 							.catch((err) => {
 								res.json({ ...req.json, err });
@@ -112,8 +122,118 @@ router.get("/user/:userId", (req, res, next) => {
 		});
 });
 
-router.patch("/user", (req, res, next) => {});
+router.patch("/user", (req, res, next) => {
+	const id = req.body.id;
+	var isVerified = false;
+	axios
+		.post(`${process.env.BASE_URL}/user/validate`, req.body)
+		.then((result) => {
+			isVerified = result.data.isDataVerified;
+			if (isVerified) {
+				User.findOne({ email: req.body.email })
+					.then((user) => {
+						if (user == null || user._id.toString() === req.body.id) {
+							bcrypt.hash(req.body.password, 12).then((hashedPassword) => {
+								User.updateOne(
+									{ id: id },
+									{ ...req.body, password: hashedPassword }
+								)
+									.then((result) => {
+										res.json({
+											...req.json,
+											isSuccessful: true,
+											wasFound: true,
+											wasUpdated: true,
+											status: CRUD_STATUS.UPDATED,
+										});
+									})
+									.catch((err) => res.json({ ...req.json, err }));
+							});
+						} else {
+							res.json({
+								...req.json,
+								wasFound: true,
+								err: "Email already in use",
+							});
+						}
+					})
+					.catch((err) => {
+						res.json({ ...req.json, err });
+					});
+			} else {
+				res.json({ ...req.json, err: result.data.err });
+			}
+		})
+		.catch((err) => {
+			res.json({ err: err });
+		});
+});
 
-router.put("/user", (req, res, next) => {});
+router.put("/user", (req, res, next) => {
+	const id = req.body.id;
+	User.findById(id)
+		.then((user) => {
+			User.deleteOne({ _id: id })
+				.then((result) => {
+					if (result.deletedCount == 0) {
+						res.json({
+							...req.json,
+							isSuccessful: true,
+							status: "No record to delete",
+						});
+					} else {
+						res.json({
+							...req.json,
+							isSuccessful: true,
+							wasFound: true,
+							wasDeleted: true,
+							status: CRUD_STATUS.DELETED,
+						});
+					}
+				})
+				.catch((err) => {
+					res.json({ ...req.json, err });
+				});
+		})
+		.catch((err) => {
+			res.json({ ...req.json, err });
+		});
+});
+
+router.get("/user/recipes/:userId", (req, res, next) => {
+	const id = req.params.userId;
+	User.findById(id)
+		.then((user) => {
+			if (user == null) {
+				res.json({ ...req.json });
+			} else {
+				let recipeIds = user.recipes;
+				if (user.recipes.length > 0) {
+					let recipes = [];
+					recipeIds.forEach((r) => {
+						Recipe.findById(r.toString())
+							.then((recipe) => {
+								recipes.push(recipe);
+							})
+							.catch((err) => {
+								console.log(
+									`Could not find recipe with an id of ${r.toString()}`
+								);
+							});
+					});
+
+					res.json({
+						...req.json,
+						isSuccessful: recipes.length > 0,
+						wasFound: isSuccessful,
+						status: isSuccessful ? CRUD_STATUS.RETRIEVED : CRUD_STATUS.ERROR,
+					});
+				}
+			}
+		})
+		.catch((err) => {
+			res.json({ ...req.json, err });
+		});
+});
 
 module.exports = router;
