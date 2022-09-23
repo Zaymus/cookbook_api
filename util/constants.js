cc651e29505aa47259cdca4f3077e1289cd42df2
Carson Brown <carsonbrown47@gmail.com>
2022-09-23 13:34:28 -0400

    added measurements array constants and objects + export cleanup

--- util/constants.js
+++ util/constants.js
@@ -41,9 +41,76 @@ const MACRO_CONSTANTS = {
 	PROTEIN_CALORIES_GRAM: 4,
 };
 
-exports.CRUD_STATUS = CRUD_STATUS;
-exports.SERVER_STATUS = SERVER_STATUS;
-exports.userValidation = userValidation;
-exports.recipeVisibility = recipeVisibility;
-exports.nutritionix_config_dev = nutritionix_config_dev;
-exports.MACRO_CONSTANTS = MACRO_CONSTANTS;
+const ACCEPTED_MEASUREMENTS = [
+	"tsp",
+	"tbsp",
+	"cup",
+	"ml",
+	"l",
+	"oz",
+	"g",
+	"mg",
+	"kg",
+];
+
+const MEASURE_AMOUNTS = {
+	EIGTH: "0.125",
+	QUARTER: "0.25",
+	HALF: "0.5",
+	ONE: "1",
+};
+
+const WEIGHT_UNITS = {
+	OUNCE: "oz",
+	GRAM: "g",
+	KILOGRAM: "kg",
+	MILLIGRAM: "mg",
+};
+
+const FLUID_UNITS = {
+	MILLILITRE: "ml",
+	LITRE: "l",
+	CUP: "cup",
+	TEASPOON: "tsp",
+	TABLESPOON: "tbsp",
+};
+
+const GRAMS_OUNCE_DIVISOR = 28.35;
+const GRAMS_K_M_CONST = 1000;
+const MILLILITRE_TEASPOON_CONST = 4.929;
+const MILLILITRE_TABLESPOON_CONST = 14.787;
+const MILLILITRE_CUP_CONST = 236.6;
+const TEASPOON_TABLESPOON_CONST = 3;
+const TEASPOON_CUP_CONST = 48;
+const TABLESPOON_MILLILITRE_CONST = 14.787;
+const TABLESPOON_CUP_CONST = 16;
+const MILLILITRE_OUNCE_CONST = 29.574;
+const TEASPOON_OUNCE_CONST = 6;
+const TABLESPOON_OUNCE_CONST = 2;
+const CUP_OUNCE_CONST = 8.115;
+
+module.exports = {
+	CRUD_STATUS,
+	SERVER_STATUS,
+	userValidation,
+	recipeVisibility,
+	nutritionix_config_dev,
+	MACRO_CONSTANTS,
+	ACCEPTED_MEASUREMENTS,
+	WEIGHT_UNITS,
+	FLUID_UNITS,
+	MEASURE_AMOUNTS,
+	GRAMS_OUNCE_DIVISOR,
+	GRAMS_K_M_CONST,
+	MILLILITRE_TEASPOON_CONST,
+	MILLILITRE_TABLESPOON_CONST,
+	MILLILITRE_CUP_CONST,
+	TEASPOON_TABLESPOON_CONST,
+	TEASPOON_CUP_CONST,
+	TABLESPOON_MILLILITRE_CONST,
+	TABLESPOON_CUP_CONST,
+	MILLILITRE_OUNCE_CONST,
+	TEASPOON_OUNCE_CONST,
+	TABLESPOON_OUNCE_CONST,
+	CUP_OUNCE_CONST,
+};
